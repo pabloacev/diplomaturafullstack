4184a5bc670632e189d196052223694f61ab0271
Pablo Acevedo <pabloacev@gmail.com>
2023-06-21 18:03:16 -0300

    PROYECTO REACT PARTE1 +++update

--- PROYECTO/front/src/components/layout/Header.js
+++ PROYECTO/front/src/components/layout/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import '../../styles/soporte.css';
 const Header = (props) => {
     return (
        
+
+
+<header>
             <div class="holder">
                  <img src="images/logonet2.jpg" />
                     <h1>Soporte Tecnico de Redes</h1>
             </div>
+
+            </header>
+
+        
         
     );
 }
