f4fc077956af5fe059b11a5b66f9a25a3ace97de
Pablo Acevedo <pabloacev@gmail.com>
2023-06-21 09:05:27 -0300

    PROYECTO REACT PARTE1 +update

--- PROYECTO/front/src/App.js
+++ PROYECTO/front/src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes,Route,BrowserRouter} from "react-router-dom";
 import Header from './components/layout/Header';
 import Nav from './components/layout/Nav';
 import Main from './components/layout/Main';
 import Footer from './components/layout/Footer';
 
+import NosotrosPage from './pages/NosotrosPage';
 
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <BrowserRouter>
     <Nav/>
+<Routes>
+  <Route path="nosotros" element={<NosotrosPage />}/>
+</Routes>
+</BrowserRouter>
     <Main/>
     <Footer/>
     </div>
---
+++ PROYECTO/front/src/pages/NosotrosPage.js
@@ -0,0 +1,82 @@
+import React from 'react';
+const NosotrosPage = (props) => {
+    return (
+       
+        <main class="holder">
+
+        <div class="historia">
+            <h2>Historia</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perferendis corporis temporibus? Autem
+                quae doloribus sint quas reprehenderit mollitia debitis quo, sunt officiis nesciunt voluptatibus
+                praesentium fugit dolorem libero nisi?</p>
+
+
+        </div>
+        <div class="staff">
+            <h2>Staff</h2>
+            <div class="personas">
+                <div class="persona">
+                <img src="images/nosotros/nosotros1.jpg" alt="" />
+               
+                    <h5>Pablo </h5>
+                    <h6>Administrador de redes</h6>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea beatae excepturi ratione quis
+                        quaerat mollitia incidunt nemo obcaecati quam, dolor harum explicabo officiis reiciendis
+                        quisquam? Culpa velit cumque officiis cupiditate?</p>
+                </div>
+
+
+                <div class="persona">
+                <img src=" images/nosotros/nosotros2.jpg" alt="" />
+                    <h5>Romina</h5>
+                    <h6>Gerente de Ventas</h6>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea beatae excepturi ratione quis
+                        quaerat
+                        mollitia incidunt nemo obcaecati quam, dolor harum explicabo officiis reiciendis quisquam? Culpa
+                        velit cumque officiis cupiditate?</p>
+                </div>
+
+
+                <div class="persona">
+                <img src=" images/nosotros/nosotros3.jpg" alt="" />
+                    <h5>Martin</h5>
+                    <h6>Seguridad Informatica</h6>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea beatae excepturi ratione quis
+                        quaerat
+                        mollitia incidunt nemo obcaecati quam, dolor harum explicabo officiis reiciendis quisquam? Culpa
+                        velit cumque officiis cupiditate?</p>
+                </div>
+
+
+                <div class="persona">
+                <img src=" images/nosotros/nosotros4.jpg" alt="" />
+                    <h5>Julieta</h5>
+                    <h6>Diseño Grafico</h6>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea beatae excepturi ratione quis
+                        quaerat
+                        mollitia incidunt nemo obcaecati quam, dolor harum explicabo officiis reiciendis quisquam? Culpa
+                        velit cumque officiis cupiditate?</p>
+                </div>
+
+
+                <div class="persona">
+                <img src=" images/nosotros/nosotros5.jpg" alt=""/>
+                    <h5>Mariano</h5>
+                    <h6>Diseño de redes FTTH</h6>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea beatae excepturi ratione quis
+                        quaerat
+                        mollitia incidunt nemo obcaecati quam, dolor harum explicabo officiis reiciendis quisquam? Culpa
+                        velit cumque officiis cupiditate?</p>
+                </div>
+            </div>
+
+        </div>
+
+
+
+    </main>
+        
+    );
+}
+
+export default NosotrosPage;
