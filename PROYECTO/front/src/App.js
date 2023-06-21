40bd33735b45ac80fb6d0f59338c2cdf564a3e75
Pablo Acevedo <pabloacev@gmail.com>
2023-06-21 11:12:59 -0300

    PROYECTO REACT PARTE1 ++update

--- PROYECTO/front/src/App.js
+++ PROYECTO/front/src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 import {Routes,Route,BrowserRouter} from "react-router-dom";
 import Header from './components/layout/Header';
 import Nav from './components/layout/Nav';
-import Main from './components/layout/Main';
 import Footer from './components/layout/Footer';
-
 import NosotrosPage from './pages/NosotrosPage';
 import ServiciosPage from './pages/ServiciosPage';
 import GaleriaPage from './pages/GaleriaPage';
@@ -23,11 +21,11 @@ function App() {
     <Nav/>
 <Routes>
   <Route path="/" element={<HomePage />}/>
-  <Route path="nosotros" element={<NosotrosPage />}/>
-  <Route path="servicios" element={<ServiciosPage />}/>
-  <Route path="galeria" element={<GaleriaPage />}/>
-  <Route path="novedades" element={<NovedadesPage />}/>
-  <Route path="contacto" element={<ContactoPage />}/>
+  <Route path="/nosotros" element={<NosotrosPage />}/>
+  <Route path="/servicios" element={<ServiciosPage />}/>
+  <Route path="/galeria" element={<GaleriaPage />}/>
+  <Route path="/novedades" element={<NovedadesPage />}/>
+  <Route path="/contacto" element={<ContactoPage />}/>
 </Routes>
 </BrowserRouter>
    
